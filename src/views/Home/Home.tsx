8e02c4d6d665270f95b00575ded8fcb3fc1950f9
goldenretriever-finance <develop@goldenretriever-finance.com>
2021-06-23 17:06:05 +0900

    style削除

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -8,16 +8,7 @@ import SwapCard from 'views/Home/components/SwapCard'
 import EarnAPRCard from 'views/Home/components/EarnAPRCard'
 import EarnAssetCard from 'views/Home/components/EarnAssetCard'
 
-const Page = styled.div`
-  align-items: center;
-  background-image: url('/images/city.png');
-  background-repeat: no-repeat;
-  background-position: bottom center;
 
-  ${({ theme }) => theme.mediaQueries.lg} {
-    padding-top: 0;
-  }
-`
 const Hero = styled.div`
   align-items: center;
   display: flex;
