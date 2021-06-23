7ddccc574ee4c7d2ff8ba21edba4f366b8f8bad8
goldenretriever-finance <develop@goldenretriever-finance.com>
2021-06-23 15:41:05 +0900

    test削除

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -6,8 +6,7 @@ const Container = styled.div`
   max-width: 1200px;
   padding-left: 16px;
   padding-right: 16px;
-  background: rgb(152,30,88);
-background: linear-gradient(0deg, rgba(152,30,88,1) 0%, rgba(25,18,65,1) 28%, rgba(7,20,65,1) 63%, rgba(0,17,75,1) 100%);
+  
 
   ${({ theme }) => theme.mediaQueries.sm} {
     padding-left: 24px;
