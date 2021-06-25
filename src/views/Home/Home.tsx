1e7b0e1b1967460219ed5334bbce602c970d86cf
goldenretriever-frontend <develop@goldenretriever-finance.com>
2021-06-25 14:08:16 +0900

    add fetch farms and TVL data

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Page from 'components/layout/Page'
+import StatusArea from "./components/StatusArea";
 
 const AnimeBg = styled.div`
   background-image: url('/images/city.png');
@@ -45,41 +46,6 @@ const HomeMv = styled.div`
     width: 48px;
     display: inline-block;
     margin-right: 10px;
-  }
-`
-
-const StatusArea = styled.div`
-  .box_wrap {
-    text-align: center;
-    width: 19%;
-    background-color: #0C0026;
-    border-radius: 6px;
-    padding: 10px;
-    box-shadow: 0 0 16px rgb(7 0 27 / 20%);
-  }
-
-  .btn_gr {
-    display: block;
-    height: 36px;
-    line-height: 31px;
-    color: #ffffff;
-    background-color: #000000;
-    border: double 2px transparent;
-    border-radius: 5px;
-    background-image: linear-gradient(#000000, #000000), linear-gradient(135deg, #0947E7, #CF00F0);
-    background-origin: border-box;
-    background-clip: content-box, border-box;
-  }
-
-  .top_area {
-    min-height: 86px;
-  }
-
-  h3 {
-    font-size: 18px;
-    font-weight: 600;
-    margin-top: 5px;
-    margin-bottom: 5px;
   }
 `
 
@@ -288,44 +254,7 @@ const Home: React.FC = () => {
               <div className="dogimage"><img src="/images/jump.png" alt="Golden retriever"/></div>
             </div>
           </HomeMv>
-          <StatusArea className="flex mt50">
-            <div className="box_wrap txt_center">
-              <div className="top_area">
-                <h3>Exchange</h3>
-                <div className="text">Exchange tokens</div>
-              </div>
-              <a href="/swap" className="btn_s btn_gr bold">Swap</a>
-            </div>
-            <div className="box_wrap txt_center">
-              <div className="top_area">
-                <h3>Liquidity</h3>
-                <div className="text">Make LP</div>
-              </div>
-              <a href="/swap" className="btn_s btn_gr bold">Add LP</a>
-            </div>
-            <div className="box_wrap txt_center">
-              <div className="top_area">
-                <h3>Farms</h3>
-                <div className="text">Stake Liquidity Pool (LP)</div>
-                <div className="nomber bold text_l">$183,213,017</div>
-              </div>
-              <a href="/swap" className="btn_s btn_gr bold">Farms</a>
-            </div>
-            <div className="box_wrap txt_center">
-              <div className="top_area">
-                <h3>Pools</h3>
-                <div className="text">Just stake some tokens to earn.</div>
-              </div>
-              <a href="/swap" className="btn_s btn_gr bold">Pools</a>
-            </div>
-            <div className="box_wrap txt_center">
-              <div className="top_area">
-                <h3>TVL</h3>
-                <div className="text">Across all LPs and Golden Retriever Pools</div>
-              </div>
-              <div className="nomber bold text_l">$183,213,017</div>
-            </div>
-          </StatusArea>
+          <StatusArea />
           <HomeContent>
             <Section1 className="mt50">
               <h2 className="txt_center"><span className="line mrauto"/>What is Golden Retriever finance?</h2>
---
+++ src/views/Home/components/StatusArea.tsx
@@ -0,0 +1,125 @@
+import React, {FC, useEffect, useMemo, useState} from "react";
+import styled from "styled-components";
+import BigNumber from "bignumber.js";
+import max from "lodash/max";
+import {getFarmApr} from "../../../utils/apr";
+import {useFarms, usePriceCakeBusd} from "../../../state/hooks";
+import {useAppDispatch} from "../../../state";
+import {fetchFarmsPublicDataAsync, nonArchivedFarms} from "../../../state/farms";
+import {useGetStats} from "../../../hooks/api";
+
+const Area = styled.div`
+  .box_wrap {
+    text-align: center;
+    width: 19%;
+    background-color: #0C0026;
+    border-radius: 6px;
+    padding: 10px;
+    box-shadow: 0 0 16px rgb(7 0 27 / 20%);
+  }
+
+  .btn_gr {
+    display: block;
+    height: 36px;
+    line-height: 31px;
+    color: #ffffff;
+    background-color: #000000;
+    border: double 2px transparent;
+    border-radius: 5px;
+    background-image: linear-gradient(#000000, #000000), linear-gradient(135deg, #0947E7, #CF00F0);
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+  }
+
+  .top_area {
+    min-height: 86px;
+  }
+
+  h3 {
+    font-size: 18px;
+    font-weight: 600;
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+`
+
+const StatusArea:FC = () => {
+  const [, setIsFetchingFarmData] = useState(true)
+  const { data: farmsLP } = useFarms()
+  const cakePrice = usePriceCakeBusd()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const fetchFarmData = async () => {
+      try {
+        await dispatch(fetchFarmsPublicDataAsync(nonArchivedFarms.map((nonArchivedFarm) => nonArchivedFarm.pid)))
+      } finally {
+        setIsFetchingFarmData(false)
+      }
+    }
+    fetchFarmData()
+  }, [dispatch, setIsFetchingFarmData])
+
+  const highestApr = useMemo(() => {
+    if (cakePrice.gt(0)) {
+      const aprs = farmsLP.map((farm) => {
+        // Filter inactive farms, because their theoretical APR is super high. In practice, it's 0.
+        if (farm.pid !== 0 && farm.multiplier !== '0X' && farm.lpTotalInQuoteToken && farm.quoteToken.busdPrice) {
+          const totalLiquidity = new BigNumber(farm.lpTotalInQuoteToken).times(farm.quoteToken.busdPrice)
+          return getFarmApr(new BigNumber(farm.poolWeight), cakePrice, totalLiquidity)
+        }
+        return null
+      })
+
+      const maxApr = max(aprs)
+      return maxApr?.toLocaleString('en-US', { maximumFractionDigits: 2 })
+    }
+    return null
+  }, [cakePrice, farmsLP])
+
+  const data = useGetStats()
+  const tvl = data ? data.tvl.toLocaleString('en-US', { maximumFractionDigits: 0 }) : null
+
+  return (
+    <Area className="flex mt50">
+      <div className="box_wrap txt_center">
+        <div className="top_area">
+          <h3>Exchange</h3>
+          <div className="text">Exchange tokens</div>
+        </div>
+        <a href="/swap" className="btn_s btn_gr bold">Swap</a>
+      </div>
+      <div className="box_wrap txt_center">
+        <div className="top_area">
+          <h3>Liquidity</h3>
+          <div className="text">Make LP</div>
+        </div>
+        <a href="/swap" className="btn_s btn_gr bold">Add LP</a>
+      </div>
+      <div className="box_wrap txt_center">
+        <div className="top_area">
+          <h3>Farms</h3>
+          <div className="text">Stake Liquidity Pool (LP)</div>
+          <div className="nomber bold text_l">{highestApr}%</div>
+        </div>
+        <a href="/swap" className="btn_s btn_gr bold">Farms</a>
+      </div>
+      <div className="box_wrap txt_center">
+        <div className="top_area">
+          <h3>Pools</h3>
+          <div className="text">Just stake some tokens to earn.</div>
+        </div>
+        <a href="/swap" className="btn_s btn_gr bold">Pools</a>
+      </div>
+      <div className="box_wrap txt_center">
+        <div className="top_area">
+          <h3>TVL</h3>
+          <div className="text">Across all LPs and Golden Retriever Pools</div>
+        </div>
+        <div className="nomber bold text_l">${tvl}</div>
+      </div>
+    </Area>
+  )
+}
+
+export default StatusArea
